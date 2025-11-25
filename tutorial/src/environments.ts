c8173dfdd9f3969c0bc837dcacc1daf51e082178
AndreaRutigliano <168710367+RutySenior@users.noreply.github.com>
2025-11-25 10:09:38 +0000

    Tutorial iniziale

---
+++ tutorial/src/environments.ts
@@ -0,0 +1,11 @@
+export const environment = {
+ firebase: {
+    apiKey: "AIzaSyCkAZxtPiTdXSAeYADVyDl4eNkIfWnX-KU",
+    authDomain: "tutorial-d987d.firebaseapp.com",
+    projectId: "tutorial-d987d",
+    storageBucket: "tutorial-d987d.firebasestorage.app",
+    messagingSenderId: "687168675226",
+    appId: "1:687168675226:web:234da0810cad2c988d2e23",
+    measurementId: "G-GRJBGWZRL1"
+ }
+};
